70b891a0a29c989adc52997cbf8726a73adcb99d
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2023-08-21 15:02:31 +0200

    DataM 21/08

--- DataManager.php
+++ DataManager.php
@@ -5,24 +5,76 @@ include_once('dataObjet/Langage.php');
 include_once('dataObjet/Projet.php');
 class DataManager
 {
+    private function connect()
+    {
+    
+        try{
+            $db = new PDO('mysql:host=localhost;dbname=cv_php_db;charset=utf8','root','',
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }catch(Exception $e){
+            die('Erreur: '.$e->getMessage());
+        }
+        return $db;
+    }
+
+    public function AddEtreprises($name, $adress)
+    {
+        try
+        {
+            $info = [$name, $adress];
+
+            $s = "INSERT INTO entreprise(name_entr, adr_entr)
+            VALUE (?,?)";
+            $pre = $this->connect()->prepare($s);
+            $pre->execute($info);
+        }
+        catch(Exception $e)
+        {
+            die('Erreur: '.$e->getMessage());
+        }
+    }
+
     public function GetAllEtreprises()
     {
-        $result = [
-            ['id_entre' => '0', 'name_entre' => 'nomEntre0', 'adr_entre' => 'Adress0'],
-            ['id_entre' => '1', 'name_entre' => 'nomEntre1', 'adr_entre' => 'Adress1'],
-            ['id_entre' => '2', 'name_entre' => 'nomEntre2', 'adr_entre' => 'Adress2'],
-            ['id_entre' => '3', 'name_entre' => 'nomEntre3', 'adr_entre' => 'Adress3']
-        ];
+        $s = "SELECT * FROM entreprise";
+
+        $pre = $this->connect()->query($s);
 
         $entreprises = [];
-        foreach($result as $key=>$row){
-            $entreprises[] = new Entreprise($row['id_entre'], $row['name_entre'], $row['adr_entre']);
+        foreach($pre->fetchAll() as $key=>$row){
+            $entreprises[] = new Entreprise($row['id_entr'], $row['name_entr'], $row['adr_entr']);
         }
         return $entreprises;
     }
     public function GetEtreprise($idEntr)
     {
+        $s = "SELECT * FROM entreprise
+        WHERE id_entr = $idEntr";
+        $pre = $this->connect()->query($s);
+        $result = fetch();
 
+        return new Entreprise($result['id_entr'], $result['name_entr'], $result['adr_entr']);
+    }
+
+    public function AddExp($titre, $idEntr, $start, $end, $tache, Array $idsLang)
+    {
+        $s = "INSERT INTO experience( titre_exp, start_exp, end_exp, tache_exp, id_entr_exp)
+            VALUES (?,?,?,?,?)";
+        $pre = $this->connect()->prepare($s);
+        $pre->execute([$titre, $start, $end, $tache, $idEntr]);
+
+        $s = "SELECT LAST_INSERT_ID()";
+        $idExp = $this->connect()->query($s)->fetch();
+
+        $s = "INSERT INTO utilise(id_lang_ut, id_exp_ut) VALUES";
+        $fl = true;
+        foreach($idsLang as $key=>$row)
+        {
+            if(!$fl){$s += ", ";}
+
+            $s += "($row, $idExp)";
+        }
+        $pre = $this->connect()->query($s);
     }
 
     public function GetAllExp()
--- sql/TablesScript.sql
+++ sql/TablesScript.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS langage(
-    id_lang int PRIMARY KEY auto_increment,
+    id_lang INT PRIMARY KEY auto_increment,
     name_lang VARCHAR(50) NOT NULL UNIQUE,
     lev_lang INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS entreprise(
-    id_entr PRIMARY KEY auto_increment,
-    name_entre VARCHAR(50) NOT NULL UNIQUE,
+    id_entr INT PRIMARY KEY auto_increment,
+    name_entr VARCHAR(50) NOT NULL UNIQUE,
     adr_entr VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS experience(
-    id_exp PRIMARY KEY auto_increment,
+    id_exp INT PRIMARY KEY auto_increment,
     titre_exp VARCHAR(50) NOT NULL,
     start_exp DATE NOT NULL UNIQUE,
     end_exp DATE NOT NULL UNIQUE,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS experience(
 );
 
 CREATE TABLE IF NOT EXISTS projet(
-    id_projet PRIMARY KEY auto_increment,
+    id_projet INT PRIMARY KEY auto_increment,
     titre_projet VARCHAR(50) NOT NULL UNIQUE,
     text_projet VARCHAR(500) NOT NULL,
     img_projet VARCHAR(50) NOT NULL UNIQUE,
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS projet(
 );
 
 CREATE TABLE IF NOT EXISTS utilise(
-    id_ut PRIMARY KEY auto_increment,
+    id_ut INT PRIMARY KEY auto_increment,
     id_lang_ut INT NOT NULL,
     id_exp_ut INT NULL,
     id_projet_ut INT NULL,
