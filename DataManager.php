8cc39fc5b6d774320989c7543e77fb0f4a4487ff
BrivaelFer <77669345+BrivaelFer@users.noreply.github.com>
2023-08-23 17:00:08 +0200

    DataManage 23/08

--- DataManager.php
+++ DataManager.php
@@ -3,8 +3,11 @@ include_once('dataObjet/Entreprise.php');
 include_once('dataObjet/Experience.php');
 include_once('dataObjet/Langage.php');
 include_once('dataObjet/Projet.php');
+
+/**Récupérassion et insertion des donnée de la DB*/
 class DataManager
 {
+    /**Connection à la DB*/
     private function connect()
     {
     
@@ -16,7 +19,7 @@ class DataManager
         }
         return $db;
     }
-
+    /**Ajout d'entreprise */
     public function AddEtreprises($name, $adress)
     {
         try
@@ -33,7 +36,11 @@ class DataManager
             die('Erreur: '.$e->getMessage());
         }
     }
-
+    /**
+     * Récupération des donnée des entreprises
+     * 
+     * Retrune une table d'objets Entreprise
+     */
     public function GetAllEtreprises()
     {
         $s = "SELECT * FROM entreprise";
@@ -42,10 +49,21 @@ class DataManager
 
         $entreprises = [];
         foreach($pre->fetchAll() as $key=>$row){
-            $entreprises[] = new Entreprise($row['id_entr'], $row['name_entr'], $row['adr_entr']);
+            $entreprises[] = new Entreprise(
+                $row['id_entr'], 
+                $row['name_entr'], 
+                $row['adr_entr']
+            );
         }
         return $entreprises;
     }
+    /**
+     * argument : idExp = id d'une entreprise
+     * 
+     * Récupération des donnée d'une entreprise
+     * 
+     * Retrune un objet Entreprise
+     */
     public function GetEtreprise($idEntr)
     {
         $s = "SELECT * FROM entreprise
@@ -79,19 +97,113 @@ class DataManager
 
     public function GetAllExp()
     {
-        
+        $s = "SELECT * FROM experience";
+
+        $pre = $this->connect()->query($s);
+
+        $exps = [];
+        foreach($pre->fetchAll() as $key=>$row){
+            $exps[] = new Experience(
+                $row['id_exp'], 
+                $row['titre_exp'], 
+                $row['start_exp'],
+                $row['end_exp'],
+                $row['tache_exp'],
+                GetEtreprise($row['id_entr_exp']),
+                GetExpLang($row['id_exp'])
+            );
+        }
+        return $entreprises;
     }
+
     public function GetExp($idExp)
     {
+        $s = "SELECT * FROM experience
+        WHERE id_Exp = $idExp";
 
+        $exp = $this->connect()->query($s)->fetch();
+
+        return new Experience(
+            $exp['id_exp'], 
+            $exp['titre_exp'], 
+            $exp['start_exp'],
+            $exp['end_exp'],
+            $exp['tache_exp'],
+            GetEtreprise($exp['id_entr_exp']),
+            GetExpLang($exp['id_exp'])
+        );
     }
+
+    public function AddLangage($name, $level)
+    {
+        $s = "INSERT INTO langage(name_lang, lev_lang)
+        VALUE (?,?)";
+
+        $pre = $this->connect()->prepare($s);
+        $pre->execute([$name, $level]);
+    }
+
     public function GetAllLangage()
     {
+        $s = "SELECT * FROM langage";
 
+        $pre = $this->connect()->query($s);
+
+        $lang = [];
+        foreach($pre->fetchAll() as $key=>$row){
+            $lang[] = new Langage($row['id_lang'], $row['name_lang'], $row['lev_lang']);
+        }
+        return $lang;
     }
+
     public function GetLangage($idLang)
     {
+        $s = "SELECT * FROM langage
+        WHERE id_lang = $idLang";
 
+        $lang = $this->connect()->query($s)->fetch();
+
+        return new Langage(
+            $lang['id_lang'], 
+            $lang['name_lang'],
+            $lang['lev_lang']
+        );
+    }
+
+    private function GetExpLang($idExp)
+    {
+        $s = "SELECT * FROM langage INNER JOIN utilise ON id_lang = id_lang_ut
+        WHERE id_exp_ut = $idExp";
+
+        $pre = $this->connect()->query($s);
+
+        $langs = [];
+        foreach($pre->fetchAll() as $key=>$row)
+        {
+            $lang = new Langage(
+                $row['id_lang'],
+                $row['name_lang'],
+                $row['lev_lang']
+            );
+        }
+    }
+
+    private function GetProjetLang($idPro)
+    {
+        $s = "SELECT * FROM langage INNER JOIN utilise ON id_lang = id_lang_ut
+        WHERE id_projet_ut = $idPro";
+
+        $pre = $this->connect()->query($s);
+        
+        $langs = [];
+        foreach($pre->fetchAll() as $key=>$row)
+        {
+            $lang = new Langage(
+                $row['id_lang'],
+                $row['name_lang'],
+                $row['lev_lang']
+            );
+        }
     }
 }
 
